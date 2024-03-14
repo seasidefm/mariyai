6503496132fe4ffd6d4410198ab694643b784828
Doug <doug@dougflynn.dev>
2024-03-14 00:47:53 -0700

    fix(scale): temporarily disable mounts
    Idk how to make mounts work nicely with orthogonal cameras

--- src/promotions/generic.ts
+++ src/promotions/generic.ts
@@ -1,4 +1,4 @@
-import { BasePromo } from './base.ts'
+import { BasePromo, RewardTier } from './base.ts'
 
 export class GenericPromo extends BasePromo {
     public getPromo() {
@@ -6,6 +6,9 @@ export class GenericPromo extends BasePromo {
     }
 
     public getActive() {
+        // TODO: Disable this line to re-enable mounts
+        return false
+
         // Active in December, before the 26th
         const now = new Date()
         const month = now.getMonth()
